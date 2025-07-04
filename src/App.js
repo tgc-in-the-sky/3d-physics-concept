4e271bb4466148a629d48289c0dd0c8802fd690e
tgc-in-the-sky <keagan@departmentsix.co>
2025-07-04 12:57:04 +0200

    Remove outlines feature and all related code/files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as THREE from "three"
 import { Canvas, useFrame, useThree } from "@react-three/fiber"
-import { Outlines, Environment, useTexture } from "@react-three/drei"
+import { Environment, useTexture } from "@react-three/drei"
 import { Physics, useSphere } from "@react-three/cannon"
 import { EffectComposer, N8AO, SMAA, Bloom } from "@react-three/postprocessing"
 import { useControls } from "leva"
@@ -28,21 +28,17 @@ const App = () => (
 )
 
 function Clump({ mat = new THREE.Matrix4(), vec = new THREE.Vector3(), ...props }) {
-  const { outlines } = useControls({ outlines: { value: 0.0, step: 0.01, min: 0, max: 0.05 } })
   const texture = useTexture("/cross.jpg")
   const [ref, api] = useSphere(() => ({ args: [1], mass: 1, angularDamping: 0.1, linearDamping: 0.65, position: [rfs(20), rfs(20), rfs(20)] }))
   useFrame((state) => {
     for (let i = 0; i < 40; i++) {
-      // Get current whereabouts of the instanced sphere
       ref.current.getMatrixAt(i, mat)
-      // Normalize the position and multiply by a negative force.
-      // This is enough to drive it towards the center-point.
       api.at(i).applyForce(vec.setFromMatrixPosition(mat).normalize().multiplyScalar(-40).toArray(), [0, 0, 0])
     }
   })
   return (
     <instancedMesh ref={ref} castShadow receiveShadow args={[sphereGeometry, baubleMaterial, 40]} material-map={texture}>
-      <Outlines thickness={outlines} />
+      {/* Outlines removed */}
     </instancedMesh>
   )
 }
